f2e22291b24258df802a13b9efd2494edcd291c5
Oleh Shynkarenko <33977283+TyraelS@users.noreply.github.com>
2018-10-20 12:33:07 +0300

    .

---
+++ src/HOC/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
